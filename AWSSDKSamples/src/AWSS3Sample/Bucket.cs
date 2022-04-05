7db82537bce14e87dd0b0fef3799166fc7e3475a
Lino <lino.sousa.86@gmail.com>
2022-04-05 19:01:11 +0100

    Added basic S3 bucket sample

---
+++ AWSSDKSamples/src/AWSS3Sample/Bucket.cs
@@ -0,0 +1,51 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.S3.Util;
+
+namespace AWSS3Sample
+{
+    public class Bucket
+    {
+        private readonly IAmazonS3 s3Client;
+        private readonly string bucketName;
+
+        public Bucket(IAmazonS3 s3Client, string bucketName)
+        {
+            this.s3Client = s3Client;
+            this.bucketName = bucketName;
+        }
+
+        public async Task<bool> Create(CancellationToken cancellationToken = default)
+        {
+            var response = await s3Client.PutBucketAsync(new PutBucketRequest
+            {
+                BucketName = bucketName,
+                UseClientRegion = true
+            }, cancellationToken);
+
+            return response.HttpStatusCode == System.Net.HttpStatusCode.OK;
+        }
+
+        public async Task<bool> Exists()
+        {
+            return await AmazonS3Util.DoesS3BucketExistV2Async(s3Client, bucketName);
+        }
+
+        public async Task<string> Location(CancellationToken cancellationToken = default)
+        {
+            var response = await s3Client.GetBucketLocationAsync(new GetBucketLocationRequest()
+            {
+                BucketName = bucketName
+            }, cancellationToken);
+
+            return response.Location.ToString();
+        }
+
+        public async Task<bool> Delete(CancellationToken cancellationToken = default)
+        {
+            var response = await s3Client.DeleteBucketAsync(bucketName, cancellationToken);
+
+            return response.HttpStatusCode == System.Net.HttpStatusCode.NoContent;
+        }
+    }
+}
---
+++ AWSSDKSamples/src/AWSSDKServices/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Amazon;
+using Amazon.Extensions.NETCore.Setup;
+using Amazon.Runtime;
+using Amazon.S3;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AWSSDKServices
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddAWSServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDefaultAWSOptions(new AWSOptions
+            {
+                Region = RegionEndpoint.GetBySystemName(configuration["AWS:Region"]),
+                Credentials = new BasicAWSCredentials(configuration["AWS:AccessKey"], configuration["AWS:SecretKey"])
+            });
+
+            services.AddAWSService<IAmazonS3>();
+
+            return services;
+        }
+    }
+}
